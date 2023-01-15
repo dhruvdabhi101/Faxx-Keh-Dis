8b08ed9c1530b116afbb5fb183d4f36697424774
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-01-15 11:39:16 +0530

    added javascript

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const openButton = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factList = document.querySelectorAll(".fact");
 
+factList.innerHTML = "";
 openButton.addEventListener("click", () => {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
